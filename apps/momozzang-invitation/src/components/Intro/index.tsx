58b0ae3abc2824a336a8cd08d66cb13c6bb1aca8
이학주 <hakjoo@daou.co.kr>
2025-08-18 14:10:05 +0900

    feat: 임시 인트로 레이어

---
+++ apps/momozzang-invitation/src/components/Intro/index.tsx
@@ -0,0 +1,20 @@
+import styles from './style.module.css';
+
+type IntroProps = {
+  onEnter?: () => void;
+};
+
+function Intro({ onEnter }: IntroProps) {
+  return (
+    <div className={styles.introWrap}>
+      <div className={styles.logo}>💌</div>
+      <h1 className={styles.title}>김철수 & 이영희의 결혼식</h1>
+      <p className={styles.date}>2025년 10월 10일 토요일 오후 1시</p>
+      <button className={styles.enterButton} onClick={onEnter}>
+        모바일 청첩장 보기
+      </button>
+    </div>
+  );
+}
+
+export default Intro;
