a512adf50a4dfec07d4ca48b8c64c96b3781e896
이학주 <hakjoo@daou.co.kr>
2025-10-14 15:33:36 +0900

    feat: react compiler 추가

--- apps/momozzang-invitation/vite.config.ts
+++ apps/momozzang-invitation/vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
